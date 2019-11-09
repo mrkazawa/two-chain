492cd9b61629609ae72e5a7960166ee8edc6e629
mrkazawa <yustus.oktian@gmail.com>
2019-11-09 16:59:48 +0900

    add login feature

--- 2fa/2fa.js
+++ 2fa/2fa.js
@@ -14,62 +14,102 @@ const app = express();
 const port = 3000;
 
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-// express api lists
-app.get('/login', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+/**
+ * Display login page.
+ */
+app.get('/login', function (request, response) {
+  response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.get('/register', function(request, response) {
-	response.sendFile(path.join(__dirname + '/register.html'));
+/**
+ * Run after submission on the login page.
+ */
+app.post('/do_login', function (request, response) {
+  var email = request.body.email;
+  var password = request.body.password;
+
+  // check if there is a value
+  if (email && password) {
+    let sql = `SELECT email FROM users WHERE email = ? and password = ?`;
+    let placeholder = [email, password]
+
+    // first row only
+    db.get(sql, placeholder, (err, row) => {
+      if (err) {
+        response.send('Fails to query data to the database!');
+        return console.error(err.message);
+      }
+
+      if (row) {
+        request.session.loggedin = true;
+				request.session.username = email;
+				response.redirect('/home');
+      } else {
+        response.send('Username or Password wrong!');
+        response.end();
+      }
+    });
+  } else {
+    response.send('Please enter Username and Password!');
+    response.end();
+  }
+});
+
+/**
+ * Display register page.
+ */
+app.get('/register', function (request, response) {
+  response.sendFile(path.join(__dirname + '/register.html'));
 });
 
 /**
  * Run after submission on the register page.
  */
-app.post('/do_register', function(request, response) {
-	var email = request.body.email;
+app.post('/do_register', function (request, response) {
+  var email = request.body.email;
   var password = request.body.password;
 
   // check if there is a value
-	if (email && password) {
+  if (email && password) {
     // insert to database
     var placeholder = [email, password]
-    db.run(`INSERT INTO users(email,password) VALUES(?,?)`, placeholder, function(err) {
+    db.run(`INSERT INTO users(email,password) VALUES(?,?)`, placeholder, function (err) {
       if (err) {
-        response.send('Fails to insert data to the database!');	
+        response.send('Fails to insert data to the database!');
         return console.log(err.message);
       }
       // get the last insert id
       console.log(`A row has been inserted with rowid ${this.lastID}`);
-      // close the database connection
-      db.close();
-      response.send('Username and Password Received!');	
+     
+      response.send('Username and Password Received!');
       response.end();
     });
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+  } else {
+    response.send('Please enter Username and Password!');
+    response.end();
+  }
 });
 
-app.get('/setting', function(request, response) {
-	response.sendFile(path.join(__dirname + '/register.html'));
+app.get('/setting', function (request, response) {
+  response.sendFile(path.join(__dirname + '/register.html'));
 });
 
-app.get('/', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
+app.get('/home', function (request, response) {
+  if (request.session.loggedin) {
+    response.send('Welcome back, ' + request.session.username + '!');
+  } else {
+    response.send('Please login to view this page!');
+  }
+  response.end();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
