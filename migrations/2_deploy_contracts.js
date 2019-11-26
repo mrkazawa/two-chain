af916fbd6cffca0782506661625f567471a8f46e
mrkazawa <yustus.oktian@gmail.com>
2019-11-26 11:33:18 +0900

    add initial contract code to store new address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 const RC = artifacts.require("RegistryContract");
-const IC = artifacts.require("ISPContract");
 
 module.exports = function(deployer) {
   deployer.deploy(RC);
-  //deployer.link(Broker, Owner);
-  deployer.deploy(IC);
-  //deployer.link(Broker, Network);
-  //deployer.deploy(Network);
 };
---
+++ test/1_add_entity_test.js
@@ -0,0 +1,32 @@
+const RegistryContract = artifacts.require("RegistryContract");
+const truffleAssert = require('truffle-assertions');
+
+contract('Gateway Authentication -- Storing Payload Test', (accounts) => {
+  const userAddress = accounts[1]; // user address
+  let RC;
+
+  beforeEach('deploy contract', async () => {
+    RC = await RegistryContract.new();
+  });
+
+  it('ANYONE can register his address', async () => {
+    let tx = await RC.storeNewEntity({
+      from: userAddress
+    });
+    truffleAssert.eventEmitted(tx, 'NewEntityAdded', {
+      sender: userAddress
+    });
+  });
+
+  it('ANYONE can NOT register his address twice', async () => {
+    await RC.storeNewEntity({
+      from: userAddress
+    });
+    // mistakenly store the same address twice
+    await truffleAssert.reverts(
+      RC.storeNewEntity({
+        from: userAddress
+      }), 'entity must not exist'
+    );
+  });
+});
